ec6bb9994250e6c5cbf3cf7fa4b1b0334feb641b
Jeongmin Lee <dlwogns9807@naver.com>
2023-02-22 21:00:28 +0900

    Programmers level0 몫 구하기

--- src/test/java/level0/나머지구하기/SolutionTest.java
+++ src/test/java/level0/나머지구하기/SolutionTest.java
@@ -1,5 +1,6 @@
-package level0.몫구하기;
+package level0.나머지구하기;
 
+import level0.몫구하기.Solution;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,16 +8,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SolutionTest {
     @Test
-    @DisplayName("10, 5 => 2")
+    @DisplayName("2, 3 => -1")
     void t1(){
-        assertThat(new Solution().solution(10, 5)).isEqualTo(2);
+        assertThat(new level0.몫구하기.Solution().solution(10, 5)).isEqualTo(2);
     }
 
     @Test
-    @DisplayName("7, 2 => 3")
+    @DisplayName("2, 3 => -1")
     void t2(){
         assertThat(new Solution().solution(7, 2)).isEqualTo(3);
     }
-
-
 }
