8cf3549d6f0ce03598bc57225f06a16bf9a1acd0
Jeongmin Lee <dlwogns9807@naver.com>
2023-02-22 20:57:12 +0900

    Programmers level0 몫 구하기

---
+++ src/test/java/level0/몫구하기/SolutionTest.java
@@ -0,0 +1,22 @@
+package level0.몫구하기;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SolutionTest {
+    @Test
+    @DisplayName("2, 3 => -1")
+    void t1(){
+        assertThat(new Solution().solution(10, 5)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("2, 3 => -1")
+    void t2(){
+        assertThat(new Solution().solution(7, 2)).isEqualTo(3);
+    }
+
+
+}
