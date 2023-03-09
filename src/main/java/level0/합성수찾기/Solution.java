cb24dd3bea07783c95b415b195b5c91c9a1b7578
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-09 13:12:20 +0900

    Programmers level0 합성수 찾기

---
+++ src/main/java/level0/합성수찾기/Solution.java
@@ -0,0 +1,20 @@
+package level0.합성수찾기;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=1; i<=n; i++){
+            int count = 0;
+            for(int j=1; j<=i; j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count>=3){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/level0/합성수찾기/SolutionTest.java
@@ -0,0 +1,21 @@
+package level0.합성수찾기;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SolutionTest {
+    @Test
+    @DisplayName("10 => 5")
+    void t1(){
+        assertThat(new Solution().solution(10)).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("15 => 8")
+    void t2(){
+        assertThat(new Solution().solution(15)).isEqualTo(8);
+    }
+}
